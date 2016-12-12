d254a27244fb1472079d9e9685c24898f12754eb
reaper <freereaper@aliyun.com>
2016-12-12 23:43:48 +0800

    add startup file.
    Signed-off-by: reaper <freereaper@aliyun.com>

---
+++ .startup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -f "$HOME/.Xresources" ]; then
+    xrdb -merge "$HOME/.Xresources"
+fi
+
+xsetroot -cursor_name left_ptr
+
+#add private fonts
+if [ -d "$HOME/.fonts" ]; then
+    xset fp+ "$HOME/.fonts"
+    xset fp rehash
+fi
+
+#make keyboard response faster
+xset r rate 200 30
+
+# enable natural scrolling and remap Caps Lock key to become left Ctrl
+if [ -f "$HOME/.Xmodmap" ]; then
+    xmodmap "$HOME/.Xmodmap"
+fi
+
+# xclip
+autocutsel -selection CLIPBOARD -fork
+autocutsel -selection PRIMARY -fork
+
+
+# set wallpaper
+if hash hsetroot 2>/dev/null; then
+    hsetroot -fill "$HOME/.blackjpg"
+fi
